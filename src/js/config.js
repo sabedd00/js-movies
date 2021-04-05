841137d594c87e489765959317330ef5cc2f72ab
xxxez44 <v.oleinyk@asedo.tech>
2021-04-05 18:08:17 +0300

    Refactoring index.js, movie-details.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,11 @@
 const API_KEY = 'api_key=efa652f89c0f139ad17f649034a7e3d4';
 const BASE_URL = 'https://api.themoviedb.org/3/';
+
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 const BIG_IMAGE_SIZE = 'w342';
 const ORIGINAL_IMAGE_SIZE = 'original';
 const SMALL_IMAGE_SIZE = 'w185';
+
 const URL_YOUTUBE = 'https://www.youtube.com/embed/';
 
 const LANGUAGE_QUERY = '&language=';
